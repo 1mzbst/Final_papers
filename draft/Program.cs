e1a7903ff2a348c25bcb6084aefc183abc89a09e
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-03 23:57:56 +0300

    Make the methods

---
+++ draft/Program.cs
@@ -0,0 +1,51 @@
+﻿// int size = 4;
+// string[] arrString = new string[size];
+// Random qwe = new Random();
+// for (int i = 0; i < size; i++)
+// {
+//     arrString[i] = Convert.ToString(qwe.Next('q', 'q' + 27));
+//     // if (arrString[i].Length <= 3)
+//     // {
+//     Console.Write(arrString[i] + ",");
+//     // }
+// }
+
+// // randoms[r.Next(0, randoms.Length)];
+// //  array[i] = new Random().Next(minValue, maxValue + 1);
+
+string[] CreateStringArr(int size)
+{
+    string[] array = new string[size];
+    Console.WriteLine($"Enter the {size} words:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+void threeLetter(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+}
+
+Console.WriteLine("Enter the size of the array.");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] someArray = CreateStringArr(size);
+ShowArray(someArray);
+threeLetter(someArray);
