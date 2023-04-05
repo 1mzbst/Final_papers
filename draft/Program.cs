d5e4e593fcb21da09c21a3f9521c6159c46a90eb
Dmitriy Kulakov <dk4git@outlook.com>
2023-04-05 19:36:08 +0300

    The draft of F_p1 is done

--- draft/Program.cs
+++ draft/Program.cs
@@ -19,7 +19,7 @@ string[] CreateStringArr(int size) // Метод создание массива
     Console.WriteLine($"Enter the {size} words:");
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = Console.ReadLine();
+        array[i] = Console.ReadLine(); // Ввод осуществляется через консоль, вручную.
         if (array[i] == "") // Условие, если пользователь пропустил ввод слова.
         {
             array[i] = "empty_index";
@@ -30,32 +30,23 @@ string[] CreateStringArr(int size) // Метод создание массива
 
 void ShowArray(string[] array) // Метод вывода массива в консоль.
 {
+    Console.Write("[ ");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + ", ");
+        Console.Write(array[i] + " ");
     }
+    Console.Write("]");
     Console.WriteLine();
 }
 
 void threeLetter(string[] array) // Метод вывода нового массива, состоящего из индексов удовлетворяющих условие задачи.
 {
-    Console.Write("This is the new array from three or less letters in a word: [");
+    Console.Write("This is the new array from three or less letters in a word: [ ");
     for (int i = 0; i < array.Length; i++)
     {
-        // if (array[i].Length <= 3)
-        // {
-        //     Console.Write(array[i] + ", ");
-        // }
-        // else if (i == array.Length - 1)
-        // {
-        //     Console.Write("ho ho ho");
-        // }
-        if (array[i].Length <= 3) Console.Write(array[i]);
-        if (i == (array.Length - 1)) Console.Write("]");
-        //Console.Write(" ");
-
+        if (array[i].Length <= 3) Console.Write($"{array[i]} ");
     }
-    //Console.Write("]");
+    Console.Write("]");
 }
 
 Console.WriteLine("Enter the size of the array.");
